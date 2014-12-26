2ad4a7bfa19455365b62369b910bf31395d762da
messense <messense@icloud.com>
2014-12-26 21:56:43 +0800

    Add markup utility

--- douban2kindle/book/models.py
+++ douban2kindle/book/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
-# Create your models here.
+
+class Book(models.Model):
+    book_id = models.CharField(max_length=200)
+    author = models.CharField(max_length=30)
+    title = models.CharField(max_length=200)
+    subtitle = models.CharField(max_length=200)
+    size = models.BigIntegerField(default=0)
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
--- douban2kindle/book/views.py
+++ douban2kindle/book/views.py
@@ -1,10 +1,46 @@
 # -*- coding: utf-8 -*-
 from __future__ import absolute_import, unicode_literals
-from django.shortcuts import render
+try:
+    import simplejson as json
+except ImportError:
+    import json
+
+from django.http import JsonResponse
 from django.views.generic import View
+from django.utils.encoding import smart_text
+
+from utils.decrypt import decrypt
 
 
 class SendView(View):
 
     def post(self, request):
+        ret_dict = {}
+
+        # Parse request data
+        to_mail = request.POST.get('toMail')
+        book_data = request.POST.get('bookData')
+
+        if not (to_mail and book_data):
+            ret_dict['status'] = 'WARN'
+            ret_dict['msg'] = '参数不能为空！'
+            return JsonResponse(ret_dict)
+
+        # Decrypt book data
+        data_str = decrypt(book_data)
+        try:
+            data = json.loads(data_str)
+        except ValueError:
+            ret_dict['status'] = 'WARN'
+            ret_dict['msg'] = '图书数据解析出错！'
+            return JsonResponse(ret_dict)
+
+        posts = data['posts'][0]
+        title = smart_text(posts.get('title', '')).strip()
+        subtitle = smart_text(posts.get('subtitle', '')).strip()
+        author = smart_text(posts.get('orig_author', '')).strip()
+        translator = smart_text(posts.get('translator', '')).strip()
+        author_id = smart_text(data.get('authorId', '')).strip()
+        book_size = len(book_data)
+
         import pdb; pdb.set_trace()
